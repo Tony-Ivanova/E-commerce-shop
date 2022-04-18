9612ae188471ab75b7861cb24bbdbc25a026c807
Tony <tony.ivanova86@gmail.com>
2022-04-18 13:45:19 +0300

    Adding error handler

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDatabase from './config/mongoDb.js'
 import ImportData from './DataImport.js'
 import router from './routes/products.js'
+import { errorHandler, notFound } from './middlewares/errors.js'
 
 dotenv.config()
 connectDatabase()
@@ -13,9 +14,7 @@ const PORT = process.env.PORT || 1000;
 
 app.use('/api/import', ImportData)
 app.use('/api/products', router)
-
-app.get('/', (req, res) => {
-    res.send('API is running')
-})
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`))
