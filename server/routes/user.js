cc5eae41a87cc34aeee0ddc4478b578c1b1c0f5d
Tony <tony.ivanova86@gmail.com>
2022-04-19 18:22:05 +0300

    User profile API

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
+import protect from '../middlewares/auth.js';
 import User from '../models/user.js';
 import generateToken from '../utils/generateToken.js';
 
@@ -22,6 +23,54 @@ router.post('/login', asyncHandler(
         } else {
             res.status(401)
             throw new Error("Invalid Email or Password")
+        }
+    })
+)
+
+router.post('/', protect, asyncHandler(
+    async (req, res) => {
+        const { email, password } = req.body
+        const userExists = await User.findOne({ email })
+
+        if(userExists){
+            res.status(400)
+            throw new Error("User already exists")
+        }
+
+        const user = await User.create({name, email, password})
+
+        if(user){
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: generateToken(user._id),
+            })
+        }
+        else{
+            res.status(400)
+            throw new Error("Invalid user data")
+        }
+
+    })
+)
+
+router.get('/profile', protect, asyncHandler(
+    async (req, res) => {
+        const user = await User.findById(req.user._id)
+
+        if (user) {
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                createdAt: user.createdAt
+            })
+        } else {
+            res.status(404)
+            throw new Error("User not found")
         }
     })
 )
