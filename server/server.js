afd799c61858f0a884a7d2e83c280e6ea516e4f1
Tony <tony.ivanova86@gmail.com>
2022-04-19 15:45:37 +0300

    Login API

--- server/server.js
+++ server/server.js
@@ -2,18 +2,21 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDatabase from './config/mongoDb.js'
 import ImportData from './DataImport.js'
-import router from './routes/products.js'
+import productRouter from './routes/products.js'
+import userRouter from './routes/user.js'
 import { errorHandler, notFound } from './middlewares/errors.js'
 
 dotenv.config()
 connectDatabase()
 
 const app = express()
+app.use(express.json())
 
 const PORT = process.env.PORT || 1000;
 
 app.use('/api/import', ImportData)
-app.use('/api/products', router)
+app.use('/api/products', productRouter)
+app.use('/api/users', userRouter)
 app.use(notFound)
 app.use(errorHandler)
 
