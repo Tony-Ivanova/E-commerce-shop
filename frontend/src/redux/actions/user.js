66051e9d453d182e7dcab7da4395d4af0c8ddb58
Tony <tony.ivanova86@gmail.com>
2022-05-01 16:11:54 +0300

    Update user profile data

--- frontend/src/redux/actions/user.js
+++ frontend/src/redux/actions/user.js
@@ -5,7 +5,11 @@ import {
     USER_LOGOUT,
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
-    USER_REGISTER_FAIL
+    USER_REGISTER_FAIL,
+    USER_DETAILS_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_RESET,
 } from "../constants/user"
 import axios from 'axios'
 
@@ -57,6 +61,39 @@ export const register = (name, email, password) => async (dispatch) => {
     }
 }
 
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: USER_DETAILS_REQUEST })
+
+        const {
+            userLogin: {
+                userInfo
+            }
+        } = getState()
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+        const { data } = await axios.get(`/api/users/${id}`, config)
+
+        dispatch({ type: USER_DETAILS_SUCCESS, payload: data })
+
+        localStorage.setItem("userInfo", JSON.stringify(data))
+    } catch (error) {
+        const message = error.response && error.response.data.message ? error.response.data.message : error.message
+
+        if (message === "Not authorized, token failed") {
+            dispatch(logout())
+        }
+        dispatch({
+            type: USER_DETAILS_FAIL,
+            payload: message
+        })
+    }
+}
 
 
 export const logout = () => (dispatch) => {
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productDetailsReducer, productListReducer } from './reducers/product'
 import { cartReducer } from './reducers/cart'
-import { userLoginReducer, userRegisterReducer } from './reducers/user'
+import { userDetailsReducer, userLoginReducer, userRegisterReducer } from './reducers/user'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -11,6 +11,7 @@ const reducer = combineReducers({
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
 })
 
 const cartItemsFromLocalStorage = localStorage.getItem('cartItems')
