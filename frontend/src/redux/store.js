bcced61875791fbf1beefb3879a1566d4966561d
Tony <tony.ivanova86@gmail.com>
2022-05-02 20:42:53 +0300

    Redux on payment

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { productDetailsReducer, productListReducer } from './reducers/product'
 import { cartReducer } from './reducers/cart'
 import { userDetailsReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer } from './reducers/user'
-import { orderCreateReducer, orderDetailsReducer } from './reducers/order'
+import { orderCreateReducer, orderDetailsReducer, orderPayReducer } from './reducers/order'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -16,6 +16,7 @@ const reducer = combineReducers({
     userUpdateProfile: userUpdateProfileReducer,
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
+    orderPay: orderPayReducer,
 })
 
 const cartItemsFromLocalStorage = localStorage.getItem('cartItems')
