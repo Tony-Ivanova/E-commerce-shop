eaa2e67e91768269f51ee449a60dbdcfe6af1c8c
Tony <tony.ivanova86@gmail.com>
2022-05-02 18:22:51 +0300

    Get order by id

--- server/routes/order.js
+++ server/routes/order.js
@@ -39,5 +39,19 @@ router.post('/', protect, asyncHandler(
     })
 )
 
+router.get('/:id', protect, asyncHandler(
+    async (req, res) => {
+        const order = await Order.findById(req.params.id).populate(
+            "user",
+            "name email"
+        )
 
+        if (order) {
+            res.json(order)
+        } else {
+            res.status(404)
+            throw new Error ("Order Not Found")
+        }
+    })
+)
 export default router
