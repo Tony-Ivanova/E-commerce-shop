782be4a14eb31854eb5a2d5c52162590cd44f610
Tony <tony.ivanova86@gmail.com>
2022-05-04 11:14:18 +0300

    Pagination

--- frontend/src/redux/actions/product.js
+++ frontend/src/redux/actions/product.js
@@ -2,20 +2,23 @@ import axios from 'axios'
 import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_CREATE_REVIEW_REQUEST, PRODUCT_CREATE_REVIEW_SUCCESS, PRODUCT_CREATE_REVIEW_FAIL } from "../constants/product";
 import { logout } from './user';
 
-export const listProduct = (keyword=" ") => async (dispatch) => {
-    try {
-        dispatch({ type: PRODUCT_LIST_REQUEST })
-        const { data } = await axios.get(`/api/products?keyword=${keyword}`)
-        dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data })
-    } catch (error) {
-        dispatch({
-            type: PRODUCT_LIST_FAIL,
-            payload:
-                error.response && error.response.data.message ? error.response.data.message : error.message,
+export const listProduct =
+    (keyword = " ", pageNumber = " ") =>
+        async (dispatch) => {
+            try {
+                dispatch({ type: PRODUCT_LIST_REQUEST })
+                const { data } = await axios.get(
+                    `/api/products?keyword=${keyword}&pageNumber=${pageNumber}`
+                ); dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+            } catch (error) {
+                dispatch({
+                    type: PRODUCT_LIST_FAIL,
+                    payload:
+                        error.response && error.response.data.message ? error.response.data.message : error.message,
 
-        })
-    }
-}
+                })
+            }
+        }
 
 export const listProductDetails = (id) => async (dispatch) => {
     try {
@@ -49,7 +52,7 @@ export const createProductReview = (productId, review) => async (dispatch, getSt
                 Authorization: `Bearer ${userInfo.token}`
             }
         }
-        
+
         await axios.post(`/api/products/${productId}/review`, review, config)
 
         dispatch({ type: PRODUCT_CREATE_REVIEW_SUCCESS })
--- frontend/src/redux/reducers/product.js
+++ frontend/src/redux/reducers/product.js
@@ -2,16 +2,21 @@ import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_
 
 export const productListReducer = (state = { products: [] }, action) => {
     switch (action.type) {
-        case PRODUCT_LIST_REQUEST:
-            return { loading: true, products: [] }
-        case PRODUCT_LIST_SUCCESS:
-            return { loading: false, products: action.payload }
-        case PRODUCT_LIST_FAIL:
-            return { loading: false, error: action.payload }
-        default:
-            return state;
+      case PRODUCT_LIST_REQUEST:
+        return { loading: true, products: [] };
+      case PRODUCT_LIST_SUCCESS:
+        return {
+          loading: false,
+          pages: action.payload.pages,
+          page: action.payload.page,
+          products: action.payload.products,
+        };
+      case PRODUCT_LIST_FAIL:
+        return { loading: false, error: action.payload };
+      default:
+        return state;
     }
-}
+  };
 
 
 export const productDetailsReducer = (state = { product: { reviews: [] } }, action) => {
