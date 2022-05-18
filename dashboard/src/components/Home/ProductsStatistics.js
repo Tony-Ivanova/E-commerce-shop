10e775cd5d89957af8ad4687177780fb03f810f8
Tony <tony.ivanova86@gmail.com>
2022-05-18 21:30:24 +0300

    Static and dashboard

--- dashboard/src/components/Home/ProductsStatistics.js
+++ dashboard/src/components/Home/ProductsStatistics.js
@@ -6,10 +6,17 @@ const ProductsStatistics = () => {
       <div className="card mb-4 shadow-sm">
         <article className="card-body">
           <h5 className="card-title">Products statistics</h5>
-          <img
-            style={{ width: "100%", height: "350px", objectFit: "contain" }}
-            src="/images/product.png"
-          />
+          <iframe
+            style={{
+              background: "#FFFFFF",
+              border: "none",
+              borderRadius: "2px",
+              boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2);",
+              width: "100%",
+              height: "350px",
+
+            }}
+            src="https://charts.mongodb.com/charts-shoeshop-gwync/embed/charts?id=62851970-dc0a-4e87-873e-58eadb3999c5&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
         </article>
       </div>
     </div>
--- dashboard/src/components/Home/SalesStatistics.js
+++ dashboard/src/components/Home/SalesStatistics.js
@@ -6,10 +6,16 @@ const SaleStatistics = () => {
       <div className="card mb-4 shadow-sm">
         <article className="card-body">
           <h5 className="card-title">Sale statistics</h5>
-          <img
-            style={{ width: "100%", height: "350px", objectFit: "contain" }}
-            src="/images/static.png"
-          />
+          <iframe
+            style={{ 
+              background: "#FFFFFF",
+              border: "none",
+              borderRadius: "2px",
+              boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2)",
+              width: "100%",
+              height: "350px", }}
+            src="https://charts.mongodb.com/charts-shoeshop-gwync/embed/charts?id=62851670-fbf5-4214-89b0-8e1b25a32da8&maxDataAge=3600&theme=light&autoRefresh=true"
+          ></iframe>
         </article>
       </div>
     </div>
